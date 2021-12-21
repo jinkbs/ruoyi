d92f81f92720faf0fb181f2fb0614bb708b78198
l1w2d3 <2327899582@qq.com>
2021-12-21 16:29:33 +0800

    add router by manully

--- src/api/login.js
+++ src/api/login.js
@@ -8,14 +8,16 @@ export function login(username, password, code, uuid) {
     code,
     uuid
   }
-  return request({
-    url: '/login',
-    headers: {
-      isToken: false
-    },
-    method: 'post',
-    data: data
-  })
+  // return request({
+  //   url: '/login',
+  //   headers: {
+  //     isToken: false
+  //   },
+  //   method: 'post',
+  //   data: data
+  // })
+   return Promise.resolve({"msg":"操作成功","code":200,"token":"eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6IjhlOGQ2ODI4LTQzYmUtNDQ1Yi1hZmU5LWVkNTlhNzIwZDVhYiJ9.LDzUBlBnXDfaJhUDh3ZOmcdivUdjSCaDkhdEEfGnbW5dPvqkMZC1PfSmVlgdKbEmVaajpA5Rzvb7tjQOKWT_qg"})
+ 
 }
 
 // 注册方法
@@ -32,10 +34,11 @@ export function register(data) {
 
 // 获取用户详细信息
 export function getInfo() {
-  return request({
-    url: '/getInfo',
-    method: 'get'
-  })
+  // return request({
+  //   url: '/getInfo',
+  //   method: 'get'
+  // })
+  return Promise.resolve({"msg":"操作成功","code":200,"permissions":["*:*:*"],"roles":["admin"],"user":{"searchValue":null,"createBy":"admin","createTime":"2021-09-09 17:25:28","updateBy":null,"updateTime":null,"remark":"管理员","params":{},"userId":1,"deptId":103,"userName":"admin","nickName":"若依","email":"ry@163.com","phonenumber":"15888888888","sex":"1","avatar":"","status":"0","delFlag":"0","loginIp":"110.86.6.58","loginDate":"2021-12-21T16:24:34.000+08:00","dept":{"searchValue":null,"createBy":null,"createTime":null,"updateBy":null,"updateTime":null,"remark":null,"params":{},"deptId":103,"parentId":101,"ancestors":null,"deptName":"研发部门","orderNum":"1","leader":"若依","phone":null,"email":null,"status":"0","delFlag":null,"parentName":null,"children":[]},"roles":[{"searchValue":null,"createBy":null,"createTime":null,"updateBy":null,"updateTime":null,"remark":null,"params":{},"roleId":1,"roleName":"超级管理员","roleKey":"admin","roleSort":"1","dataScope":"1","menuCheckStrictly":false,"deptCheckStrictly":false,"status":"0","delFlag":null,"flag":false,"menuIds":null,"deptIds":null,"admin":true}],"roleIds":null,"postIds":null,"roleId":null,"admin":true}})
 }
 
 // 退出方法
